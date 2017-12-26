b16a6dee8137ed8290dc331c042e4ba34fbfd3a0
Kipngetich33 <khalifngeno@gmail.com>
2017-12-26 03:06:00 -0500

    create test for the pitch class

---
+++ app/models/pitch.py
@@ -0,0 +1,8 @@
+class Pitch:
+    '''
+    Pitch class to define Pitch Objects
+    '''
+
+    def __init__(self,id,pitch):
+        self.id =id
+        self.pitch = pitch
---
+++ app/pitch_test.py
@@ -0,0 +1,20 @@
+import unittest
+from models import pitch
+Pitch = pitch.Pitch
+
+class PitchTest(unittest.TestCase):
+    '''
+    Test Class to test the behaviour of the Pitch class
+    '''
+
+    def setUp(self):
+        '''
+        Set up method that will run before every Test
+        '''
+        self.new_pitch = Pitch(133,'killed by excelence')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_pitch,Pitch))
+
+if __name__ == '__main__':
+    unittest.main()
